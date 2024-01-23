0ba0e78421ef3adca43d326f75c6cca208915237
ErikaFogar <erikafogar.j@gmail.com>
2024-01-23 18:17:57 +0100

    Primer commit

---
+++ hellogit.js
@@ -0,0 +1,9 @@
+console.log('Hello git');
+console.log('Que tal el curso?');
+
+//* indicar que quiero inicializar un contexto de version de controles aqui.
+//* En terminal:
+
+//* git init
+
+//! Ver ficheros y carpetas ocultos con command + shift + .
