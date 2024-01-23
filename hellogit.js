0029eda442c01671444911ff3918b43e79b5ef6e
ErikaFogar <erikafogar.j@gmail.com>
2024-01-23 19:02:44 +0100

    gitignore

--- hellogit.js
+++ hellogit.js
@@ -1,4 +1,4 @@
-console.log('Hello git');
+console.log('Ahora estoy editando el fichero');
 console.log('Que tal el curso?');
 
 //* indicar que quiero inicializar un contexto de version de controles aqui.
@@ -12,9 +12,24 @@ git add = añadir los ficheros para ser fotografiados. Area Stage
 
 git commit -m 'comentario' = lanzar la fotografia del codigo.
 
-//* Ver si la foto se hizo
+//* Ver si la foto se hizo 
+//* = git log || git log --graph || git log --graph --pretty=oneline || etc
+salgo con tecla q
 
-git log
+git checkout = para volver atras los cambios antes de hacer hacer un commit. 
+
+git reset  = volver a la ultima fotografia del fichero.
+
+//* Podemos crear nuestros propios alias
+
+
+git config --global alias.graph 'git log --graph'
+
+
+
+
+
+
 
 
 
