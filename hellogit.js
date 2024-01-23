57f0e794258f1f05582719fcbe923fcfbc893df1
ErikaFogar <erikafogar.j@gmail.com>
2024-01-23 18:41:05 +0100

    Segundo commit

--- hellogit.js
+++ hellogit.js
@@ -4,6 +4,21 @@ console.log('Que tal el curso?');
 //* indicar que quiero inicializar un contexto de version de controles aqui.
 //* En terminal:
 
-//* git init
+/* git init = inicia el proyecto
 
-//! Ver ficheros y carpetas ocultos con command + shift + .
+git status = ver que esta pasando con los ficheros
+
+git add = añadir los ficheros para ser fotografiados. Area Stage
+
+git commit -m 'comentario' = lanzar la fotografia del codigo.
+
+//* Ver si la foto se hizo
+
+git log
+
+
+
+*/
+
+//! Ver ficheros y carpetas ocultos con command + shift + . 
+
